9f9c53ca79592827e676ba738bf92cc00e04e75f
Shaik Mosin <shaikmosin0405@gmail.com>
2024-03-20 07:51:01 +0530

    second commit

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 const mongoose=require('mongoose')
-const mongoURI='mongodb://127.0.0.1:27017/BookXchange'
+// const mongoURI=process.env.CONN_STR
+// const mongoURI="mongodb+srv://admin:3MbgrcBFgmrZzAeo@cluster0.qvkkvws.mongodb.net/BookXchange?retryWrites=true&w=majority&appName=Cluster0"
+const dotenv=require('dotenv')
+dotenv.config({path:'config.env'})
+// console.log(process.env.CONN_STR)
 const connectToMongo=async ()=>{
-    await mongoose.connect(mongoURI).then(()=>{
+    await mongoose.connect(process.env.CONN_STR,{useNewUrlParser:true}).then(()=>{
         console.log('connected to Mongo')
 
     }).catch(err=>console.log(err));
