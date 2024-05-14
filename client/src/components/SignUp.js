b7e9c2be20962c2657d33775ee19cd6f1d854fa3
Shaik Mosin <73395830+Shaikmosin@users.noreply.github.com>
2024-05-14 22:45:01 +0530

    Update SignUp.js

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -9,7 +9,7 @@ const SignUp = (props) => {
         const {name,email,password}=credentials;
        
        
-        const response = await fetch(`https://book-xchange.vercel.app/api/auth/createuser`, {
+        const response = await fetch(`http://localhost:9000/api/auth/createuser`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -41,31 +41,33 @@ const SignUp = (props) => {
         setCrediantials({ ...credentials, [e.target.name]: e.target.value })
       }
     return (
-        <div className='container mt-5'>
-            <h2>SignUp to Use BookXchange Services </h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" aria-describedby="emailHelp" name="name" onChange={onChange} value={credentials.name} required />
-                    
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={onChange} value={credentials.email} required/>
-                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" name='password' onChange={onChange} value={credentials.password} required minLength={5}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="cpassword" className="form-label">Confirm Password</label>
-                    <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} value={credentials.cpassword} required minLength={5}/>
-                </div>
-               
-                <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        </div>
+        <div className='signup-container'>
+        <h2>SignUp to Use iNoteBook </h2>
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control" id="name" aria-describedby="emailHelp" name="name" onChange={onChange} value={credentials.name} required />
+                
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" className="form-control" id="email" name='email' aria-describedby="emailHelp" onChange={onChange} value={credentials.email} required/>
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="password" className="form-label">Password</label>
+                <input type="password" className="form-control" id="password" name='password' onChange={onChange} value={credentials.password} required minLength={5}/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+                <input type="password" className="form-control" id="cpassword" name='cpassword' onChange={onChange} value={credentials.cpassword} required minLength={5}/>
+            </div>
+           
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+
     )
 }
 
