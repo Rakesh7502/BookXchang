61eb8f75555f7155a56dbe73d073177c94589dbe
Shaik Mosin <73395830+Shaikmosin@users.noreply.github.com>
2024-06-07 16:58:52 +0530

    Update Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,7 +11,7 @@ const Login = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         // console.log(credentials.email)
-        const response = await fetch(`http://localhost:9000/api/auth/login`, {
+        const response = await fetch(`https://book-xchange.vercel.app/api/auth/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
