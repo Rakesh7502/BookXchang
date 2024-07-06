e738908261802958085689af7f5d61e7d48d7be5
Shaik Mosin <73395830+Shaikmosin@users.noreply.github.com>
2024-07-06 22:34:49 +0530

    Update Books.js

--- backend/models/Books.js
+++ backend/models/Books.js
@@ -1,33 +1,52 @@
-const mongoose=require('mongoose')
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const Mail = require('nodemailer/lib/mailer');
 
-const userSchema = new Schema({
-    name:{
+const bookSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description:{
         type:String,
         required:true
     },
-    phone:{
-        type:String,
-        required:true,
-        unique:true
+
+    author: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    seller: {
+        type: String,
+        required: true
 
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    seller_contact: {
+        type: String,
+        required: true
+
     },
-    password:{
+    seller_gmail:{
         type:String,
         required:true
-    },
-    date:{
-        type:Date,
-        default:Date.now
 
+    },
+    image:{
+        type:String,
+        required:true
     }
 
-  });
-const User=mongoose.model('user',userSchema)
+});
 
-module.exports=User
+module.exports = mongoose.model('Book', bookSchema);
